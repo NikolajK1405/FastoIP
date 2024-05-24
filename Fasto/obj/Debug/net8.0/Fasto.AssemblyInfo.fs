cd392e1d7af7fe29d3fc3ad3a4bdf68b798e1e5d
NikolajK1405 <Nikolajkrarup@hotmail.com>
2024-05-24 16:02:40 +0200

    scan codegen..

--- Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
+++ Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759a50b3abe37a488a39d82ea4c4dd72c501ba31")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1a7248fe7837316f151aef9f331887412dc1db")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
